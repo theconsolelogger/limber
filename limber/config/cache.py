94269de20002d67a7df24691012174ddd75eb3ad
Jonathan Staniforth <jonathanstaniforth@gmail.com>
2020-12-27 16:26:00 +0000

    Upgrade limberframework to version 0.2.0.

--- limber/config/cache.py
+++
@@ -1,7 +0,0 @@
-from typing import Optional
-from pydantic import Field
-from limberframework.config.config import BaseConfig
-
-class CacheConfig(BaseConfig):
-    driver: Optional[str] = Field(..., env='CACHE_DRIVER')
-    path: Optional[str] = Field('storage/cache', env='CACHE_PATH')
